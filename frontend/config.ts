e3739489b460d9cc383a34be5994b76ae0512229
Leap <git@leap.new>
2025-09-03 04:32:46 +0000

    Manual code change

--- frontend/config.ts
+++ frontend/config.ts
@@ -2,4 +2,4 @@
 // Set this to your Stripe publishable key (starts with "pk_"), for example: "pk_test_123...".
 // IMPORTANT: Do NOT put any secret keys (sk_...) here; secrets must only be set on the backend.
 // NEVER hardcode secret keys in the frontend.
-export const STRIPE_PUBLISHABLE_KEY = "whsec_r5Vk0MDQLNSEeLZ48xvb4ZH7Q4EjFRBf";
+export const STRIPE_PUBLISHABLE_KEY = "pk_test_51S2Y3iPUkb8apElmzxlSimxk46JIhVgilAsO9SjUMftq5CMI1sZJf6OPXfEue4GrBqChFHwr7xyEzRw9b1jJfSs100oqz5cbmS";
