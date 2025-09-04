006a825a957d7edbc06c3f48b0d993937af2911b
Leap <git@leap.new>
2025-09-04 13:31:30 +0000

    Manual code change

--- backend/store/config.ts
+++ backend/store/config.ts
@@ -1,4 +1,5 @@
 import { secret } from "encore.dev/config";
 
 export const stripeSecretKey = secret("STRIPE_SECRET_KEY");
+export const stripePublishableKey = secret("STRIPE_PUBLISHABLE_KEY")
 export const stripeWebhookSecret = secret("STRIPE_WEBHOOK_SECRET");
