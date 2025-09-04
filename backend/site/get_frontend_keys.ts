b326c45c40c602fe152346da2f71363bb5a6863b
Leap <git@leap.new>
2025-09-04 13:46:26 +0000

    Manual code change

--- backend/site/get_frontend_keys.ts
+++ backend/site/get_frontend_keys.ts
@@ -4,7 +4,7 @@ import { stripePublishableKey } from "../store/config"
 export interface GetPublishableKeyResponse {
   key: string;
 }
-
+ 
 // Expose a GET endpoint to fetch the key
 export const getPublishableKey = api<void, GetPublishableKeyResponse>(
   { expose: true, method: "GET", path: "/stripe/publishable-key" },
