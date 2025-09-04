e1b1f1d7a7b051ec67f5054407f1c7d9a562b81e
Leap <git@leap.new>
2025-09-04 14:51:19 +0000

    Manual code change

--- backend/site/get_frontend_keys.ts
+++ backend/site/get_frontend_keys.ts
@@ -9,10 +9,10 @@ export interface GetFrontendKeysResponse {
 export const getFrontendKeys = api<void, GetFrontendKeysResponse>(
   { expose: true, method: "GET", path: "/stripe/publishable-key" },
   async () => {
-		console.log("Env key on backend: ", stripePublishableKey)
-    if (!stripePublishableKey) {
+		console.log("Env key on backend: ", stripePublishableKey())
+    if (!stripePublishableKey()) {
       throw new Error("Stripe publishable key is not configured");
     }
-    return {stripePublishableKey};
+    return { stripePublishableKey() };
   }
 );
